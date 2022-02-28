0ddffbc54b1537a969ec519c392d2040673899d1
Mario Egocheaga <43833659+Mario-Egocheaga@users.noreply.github.com>
2022-02-27 20:30:19 -0500

    User Input

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -4,7 +4,19 @@ public class AnimalPicker
 {
     public static void main(String[] args)
     {
+        System.out.println("Would You Like To See A Dog or Cat?");
         Scanner anim = new Scanner(System.in);
+        String animal = anim.next().toLowerCase();
+
+        if(animal.equals("dog"))
+        {
+            System.out.println("DOG ASCII ART HERE");
+        }
+        else if (animal.equals("cat"))
+        {
+            System.out.println("CAT ASCII ART HERE");
+        }
+
 
     }
 
